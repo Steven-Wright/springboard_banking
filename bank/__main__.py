2fa4e3528799612cc5c49cc7690ea7af2a9a7135
Steven Wright <423020+Steven-Wright@users.noreply.github.com>
2022-02-04 09:54:11 -0800

    finished implementing CLI

--- bank/__main__.py
+++ bank/__main__.py
@@ -1,6 +1,10 @@
 import argparse
 from bank import Storage, FileUtils
-from bank.main import list_employees, add_employee, remove_employee,  list_customers, add_customer, remove_customer
+from bank.main import list_employees, add_employee, remove_employee
+from bank.main import list_applicaitons, approve_application, remove_application
+from bank.main import list_customers, add_customer, remove_customer
+from bank.main import list_accounts, add_account, remove_account, deposit, withdraw, transfer
+from bank.main import list_services, apply_for_service, borrow_from_service, pay_to_service
 
 parser = argparse.ArgumentParser()
 parser.description = "simulate a bank, demonstrate OOP design practices"
@@ -13,32 +17,59 @@ employee_parser = command_subparsers.add_parser('employee')
 employee_subparser = employee_parser.add_subparsers()
 employee_subparser.required = True
 
+# bank employee list
 employee_list = employee_subparser.add_parser('list')
 employee_list.set_defaults(func=list_employees)
 
+# bank employee add <first_name> <second_name>
 employee_add = employee_subparser.add_parser('add')
 employee_add.add_argument('first_name', type=str)
 employee_add.add_argument('last_name', type=str)
 employee_add.set_defaults(func=add_employee)
 
+# bank employee remove <index>
 employee_remove = employee_subparser.add_parser('remove')
 employee_remove.add_argument("employee_index", type=int)
 employee_remove.set_defaults(func=remove_employee)
+
+# EMPLOYEE APPLICATION SUB COMMAND
+employee_application_parser = employee_subparser.add_parser('application')
+employee_application_subparser = employee_application_parser.add_subparsers()
+employee_application_subparser.required = True
+
+# bank employee service list
+employee_service_list = employee_application_subparser.add_parser('list')
+employee_service_list.set_defaults(func=list_applicaitons)
+
+# bank employee service approve <customer_index> <service_index>
+employee_service_approve = employee_application_subparser.add_parser('approve')
+employee_service_approve.add_argument('customer_index', type=int)
+employee_service_approve.add_argument('service_index', type=int)
+employee_service_approve.set_defaults(func=approve_application)
+
+# bank employee service deny <customer_index> <service_index>
+employee_service_deny = employee_application_subparser.add_parser('deny')
+employee_service_deny.add_argument('customer_index', type=int)
+employee_service_deny.add_argument('service_index', type=int)
+employee_service_deny.set_defaults(func=remove_application)
 
 # CUSTOMER SUB COMMAND
 customer_parser = command_subparsers.add_parser('customer')
 customer_subparser = customer_parser.add_subparsers()
 customer_subparser.required = True
 
+# bank customer list
 customer_add = customer_subparser.add_parser("list")
 customer_add.set_defaults(func=list_customers)
 
+# bank customer add <first_name> <last_name> <address>
 customer_add = customer_subparser.add_parser('add')
 customer_add.add_argument("first_name", type=str)
 customer_add.add_argument("last_name", type=str)
 customer_add.add_argument("address", type=str)
 customer_add.set_defaults(func=add_customer)
 
+# bank customer remove <index>
 customer_remove = customer_subparser.add_parser('remove')
 customer_remove.add_argument("customer_index", type=int)
 customer_remove.set_defaults(func=remove_customer)
@@ -48,12 +79,76 @@ customer_account_parser = customer_subparser.add_parser('account')
 customer_account_subparser = customer_account_parser.add_subparsers()
 customer_account_subparser.required = True
 
+# bank customer account list <customer_index>
+customer_account_list = customer_account_subparser.add_parser('list')
+customer_account_list.add_argument("customer_index", type=int)
+customer_account_list.set_defaults(func=list_accounts)
+
+# bank customer account add <customer_index>
+customer_account_add = customer_account_subparser.add_parser('add')
+customer_account_add.add_argument("customer_index", type=int)
+customer_account_add.add_argument("type", type=str)
+customer_account_add.set_defaults(func=add_account)
+
+# bank customer account remove <customer_index> <account_index>
+customer_remove = customer_account_subparser.add_parser('remove')
+customer_remove.add_argument("customer_index", type=int)
+customer_remove.add_argument("account_index", type=int)
+customer_remove.set_defaults(func=remove_account)
+
+# bank customer account deposit <customer_index> <account_index> <amount>
 customer_account_deposit = customer_account_subparser.add_parser('deposit')
 customer_account_deposit.add_argument("customer_index", type=int)
 customer_account_deposit.add_argument("account_index", type=int)
 customer_account_deposit.add_argument("amount", type=float)
-# customer_account_deposit.set_defaults(func=deposit)
+customer_account_deposit.set_defaults(func=deposit)
 
+# bank customer account withdraw <customer_index> <account_index> <amount>
+customer_account_withdraw = customer_account_subparser.add_parser('withdraw')
+customer_account_withdraw.add_argument("customer_index", type=int)
+customer_account_withdraw.add_argument("account_index", type=int)
+customer_account_withdraw.add_argument("amount", type=float)
+customer_account_withdraw.set_defaults(func=withdraw)
+
+# bank customer account transfer <customer_index> <source_account_index> <destination_account_index> <amount>
+customer_account_transfer = customer_account_subparser.add_parser('transfer')
+customer_account_transfer.add_argument("customer_index", type=int)
+customer_account_transfer.add_argument("source_account_index", type=int)
+customer_account_transfer.add_argument("destination_account_index", type=int)
+customer_account_transfer.add_argument("amount", type=float)
+customer_account_transfer.set_defaults(func=transfer)
+
+# # CUSTOMER SERVICE SUB COMMAND
+customer_service_parser = customer_subparser.add_parser('service')
+customer_service_subparser = customer_service_parser.add_subparsers()
+customer_service_subparser.required = True
+
+# bank customer service list <customer_index>
+customer_service_list = customer_service_subparser.add_parser('list')
+customer_service_list.add_argument("customer_index", type=int)
+customer_service_list.set_defaults(func=list_services)
+
+# bank customer service apply <customer_index> <limit>
+customer_service_apply = customer_service_subparser.add_parser('apply')
+customer_service_apply.add_argument("customer_index", type=int)
+customer_service_apply.add_argument("limit", type=float)
+customer_service_apply.set_defaults(func=apply_for_service)
+
+# bank customer service borrow <customer_index> <service_index> <account_index> <amount>
+customer_service_borrow = customer_service_subparser.add_parser('borrow')
+customer_service_borrow.add_argument("customer_index", type=int)
+customer_service_borrow.add_argument("service_index", type=int)
+customer_service_borrow.add_argument("account_index", type=int)
+customer_service_borrow.add_argument("amount", type=float)
+customer_service_borrow.set_defaults(func=borrow_from_service)
+
+# bank customer service pay <customer_index> <service_index> <account_index> <amount>
+customer_service_pay = customer_service_subparser.add_parser('pay')
+customer_service_pay.add_argument("customer_index", type=int)
+customer_service_pay.add_argument("service_index", type=int)
+customer_service_pay.add_argument("account_index", type=int)
+customer_service_pay.add_argument("amount", type=float)
+customer_service_pay.set_defaults(func=pay_to_service)
 
 arguments = parser.parse_args()
 with Storage(FileUtils, arguments.file) as storage:
--- bank/main.py
+++ bank/main.py
@@ -1,6 +1,6 @@
 import logging
 import sys
-from bank import Employee, Customer
+from bank import Employee, Customer, Account, Service
 
 
 def _get_customer(storage, index):
@@ -14,17 +14,30 @@ def _get_customer(storage, index):
     return customer
 
 
-def _get_account(storage, customer_index, account_index):
+def _get_customer_account(storage, customer_index, account_index):
     """ helper function to retrieve account from storage """
     customer = _get_customer(storage, customer_index)
     try:
-        account = customer[account_index]
+        account = customer.accounts[account_index]
     except IndexError:
         logging.critical(
             "Could not find account %s belonging to customer %s", account_index, customer_index)
         sys.exit(1)
 
-    return account
+    return (customer, account)
+
+
+def _get_customer_service(storage, customer_index, service_index):
+    """ helper function to retrieve service from storage """
+    customer = _get_customer(storage, customer_index)
+    try:
+        service = customer.services[service_index]
+    except IndexError:
+        logging.critical(
+            "Could not find service %s belonging to customer %s", service_index, customer_index)
+        sys.exit(1)
+
+    return (customer, service)
 
 
 def list_employees(storage, _):
@@ -72,28 +85,135 @@ def remove_customer(storage, args):
     else:
         del storage.customers[args.customer_index]
 
-# def open_account(args):
-#     """ add account for customer """
-#     with Storage(FileUtils, "bank.json")
 
-#     with Storage(FileUtils, "bank.json") as storage:
-#         try:
-#             customer = storage.customers[args.customer_index]
-#         except IndexError:
-#             logging.critical(
+def list_accounts(storage, args):
+    cust = _get_customer(storage, args.customer_index)
+    print("Listing {0} accounts".format(len(cust.accounts)))
+    for i, account in enumerate(cust.accounts):
+        print("{0}: {1}, ${2:.2f}".format(i, account.type, account.balance))
 
-#             )
-# def deposit(args):
-#     """ deposit into a customer's account """
-#     with Storage(FileUtils, "bank.json") as storage:
 
-#         try:
-#             account = customer.accounts[args.account_index]
-#         except IndexError:
-#             logging.critical(
-#                 "Could not find account %s for customer %s",
-#                     args.account_index,
-#                     args.customer_index
-#             )
+def add_account(storage, args):
+    """ add an account to a customer """
+    cust = _get_customer(storage, args.customer_index)
+    cust.accounts.append(Account(args.type, 0))
 
-#         account.deposit(args.amount)
+
+def remove_account(storage, args):
+    """ remove an account from a customer """
+    cust, acct = _get_customer_account(
+        storage, args.customer_index, args.account_index)
+    if acct.balance != 0:
+        logging.error("Could not remove customer %s account %s, account balance is $%s not $0.00",
+                      args.customer_index, args.account_index, acct.balance)
+    else:
+        del cust.accounts[args.account_index]
+
+
+def deposit(storage, args):
+    """ deposit into a customer's account """
+    _, account = _get_customer_account(
+        storage, args.customer_index, args.account_index)
+    account.deposit(args.amount)
+
+
+def withdraw(storage, args):
+    """ withdrawl from a customer's account """
+    _, account = _get_customer_account(
+        storage, args.customer_index, args.account_index)
+    account.withdrawl(args.amount)
+
+
+def transfer(storage, args):
+    """ transfer money between a customer's accounts """
+    _, source_account = _get_customer_account(
+        storage, args.customer_index, args.source_account_index)
+    _, destination_account = _get_customer_account(
+        storage, args.customer_index, args.destination_account_index)
+
+    source_account.withdrawl(args.amount)
+    destination_account.deposit(args.amount)
+
+
+def list_services(storage, args):
+    """ list a customer's services """
+    cust = _get_customer(storage, args.customer_index)
+    print("Listing {0} services".format(len(cust.services)))
+    if len(cust.services) > 0:
+        print("index: status, limit, balance")
+    for i, service in enumerate(cust.services):
+        print("{0}: {1} ${2} ${3}".format(
+            i, service.status, service.limit, service.balance))
+
+
+def apply_for_service(storage, args):
+    """ apply for a new service """
+    cust = _get_customer(storage, args.customer_index)
+    service = Service(args.limit)
+    cust.services.append(service)
+
+
+def borrow_from_service(storage, args):
+    """ borrow funds from a service """
+    _, service = _get_customer_service(
+        storage, args.customer_index, args.service_index)
+    _, account = _get_customer_account(
+        storage, args.customer_index, args.account_index)
+    service.lend(args.amount, account)
+
+
+def pay_to_service(storage, args):
+    """ pay funds to a service """
+    _, service = _get_customer_service(
+        storage, args.customer_index, args.service_index)
+    _, account = _get_customer_account(
+        storage, args.customer_index, args.account_index)
+    service.collect(args.amount, account)
+
+
+def list_applicaitons(storage, _):
+    """ list all pending applications """
+    applications = []
+    for customer_index, customer in enumerate(storage.customers):
+        for service_index, service in enumerate(customer.services):
+            if service.status == "application":
+                applications.append((
+                    customer_index,
+                    customer.f_name,
+                    customer.l_name,
+                    customer.total_balance,
+                    customer.total_limit,
+                    service_index,
+                    service.limit))
+
+    print("Listing {0} applications".format(len(applications)))
+    if len(applications) > 0:
+        print("customer_index, first_name, last_name, total_balance, total_limit, service_index, limit")
+        for application in applications:
+            print("#{0}, {1}, {2}, {3}, ${4}, #{5}, ${6}".format(*application))
+
+
+def approve_application(storage, args):
+    """ approve an application """
+    cust, service = _get_customer_service(
+        storage, args.customer_index, args.service_index)
+    if cust.total_balance < (cust.total_limit + service.limit):
+        logging.warning("Customer %s's total balance %s is less than total limit after approval %s",
+                        args.customer_index,
+                        cust.total_balance,
+                        cust.total_limit + service.limit
+                        )
+    else:
+        service.approve()
+
+
+def remove_application(storage, args):
+    """ remove an application """
+    cust, service = _get_customer_service(
+        storage, args.customer_index, args.service_index)
+    print(service.status)
+    if service.status != "application":
+        logging.warning(
+            "cannot remove customer %s's service %s as it is not an application")
+    else:
+        del cust.services[args.service_index]
